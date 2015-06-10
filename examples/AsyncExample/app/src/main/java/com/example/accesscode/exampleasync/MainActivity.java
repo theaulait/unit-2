18d0ac29dfe645a8ac5e68d043430badc922104a
John Rodriguez <john.rodriguez@gmail.com>
2015-06-09 21:46:25 -0400

    remove unused imports

--- examples/AsyncExample/app/src/main/java/com/example/accesscode/exampleasync/MainActivity.java
+++ examples/AsyncExample/app/src/main/java/com/example/accesscode/exampleasync/MainActivity.java
@@ -5,18 +5,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
